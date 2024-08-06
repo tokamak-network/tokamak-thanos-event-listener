89f391a71e3f24dfd62cd70fcb9a959794139735
0x6e616d <nam@tokamak.network>
2024-08-06 14:54:41 +0700

    chore: remove unused constants

--- internal/pkg/constant/constant.go
+++ internal/pkg/constant/constant.go
@@ -1,24 +1,7 @@
 package constant
 
 import (
-	"strings"
-
 	"github.com/ethereum/go-ethereum/common"
-
-	"github.com/tokamak-network/tokamak-thanos-event-listener/internal/pkg/types"
-)
-
-var (
-	KnownTokensMap = map[string]types.Token{
-		strings.ToLower("0x4200000000000000000000000000000000000486"): {
-			Symbol:   "ETH",
-			Decimals: 18,
-		},
-		strings.ToLower("0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"): {
-			Symbol:   "TON",
-			Decimals: 18,
-		},
-	}
 )
 
 var ZeroHash = common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000")
